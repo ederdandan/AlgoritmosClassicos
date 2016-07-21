734539224e5466017e7a15ffc8e11afd6063b50a
coracao <coracao@BRCGHL905>
2016-07-21 12:56:33 -0300

    Mensagem

---
+++ AlgoritmosClassicos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoritmosClassicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StairCase.Run(20);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgoritmosClassicos/StairCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgoritmosClassicos
+{
+    public static class StairCase
+    {
+        public static void Run(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                var result = (String.Concat(Enumerable.Repeat(" ", n)) + String.Concat(Enumerable.Repeat("#", i)));
+                Console.WriteLine(result.Substring(result.Length - n));
+            }
+        }
+    }
+}
